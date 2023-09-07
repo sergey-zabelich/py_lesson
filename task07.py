bae665ea407bf64a0cbdfeb078c6aa2d6be53ea9
Sergey <zabelich19@gmail.com>
2023-09-07 21:16:04 +0300

    аполните массив элементами арифметической прогрессии. Её первый элемент, разность и количество элементов нужно ввести с клавиатуры. Формула для получения n-го члена прогрессии: an = a1 + (n-1) * d.Каждое число вводится с новой строки.

--- task07.py
+++ task07.py
@@ -2,14 +2,8 @@
 Задача 30:  Заполните массив элементами арифметической прогрессии. Её первый элемент, разность и количество элементов нужно ввести с клавиатуры. 
 Формула для получения n-го члена прогрессии: an = a1 + (n-1) * d.Каждое число вводится с новой строки.
 '''
-def find_indexes(lst, min_value, max_value):
-    indexes = []
-    for i in range(len(lst)):
-        if min_value <= lst[i] <= max_value:
-            indexes.append(i)
-    return indexes
-lst = [1, 5, 88, 100, 2, -4]
-min_value = 33
-max_value = 200
-result = find_indexes(lst, min_value, max_value)
-print(result)
+a1 = 7
+d = 2
+n = 5
+for i in range(n):
+    print(a1 + i * d, end=' ')
